89dc4cbd583f80b52c988756a5c5a2602fb34274
Bartek <bartlomiejtolak7@gmail.com>
2023-04-22 09:14:17 +0200

    [Change] CustomRestController to CustomRestControllerDev

--- src/main/java/pl/sda/OrangeJavaPL2/restapi/CustomRestControllerDev.java
+++ src/main/java/pl/sda/OrangeJavaPL2/restapi/CustomRestControllerDev.java
@@ -5,24 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CustomRestController {
+public class CustomRestControllerDev {
 
-    @GetMapping("/hello")
-    public String HelloDefault(){
-        return "John Rambo";
-    }
+
 
 
     @Profile("dev")
-    @GetMapping("/hello1")
+    @GetMapping("/hello")
     public String HelloDev(){
         return "John Rambo from dev";
     }
 
 
-    @Profile("prd")
-    @GetMapping("/hello2")
-    public String HelloProduction(){
-        return "John Rambo from production";
-    }
+
 }
