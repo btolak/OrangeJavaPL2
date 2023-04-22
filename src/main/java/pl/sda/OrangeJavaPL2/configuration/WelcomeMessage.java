151376dba8b089203306dd0d821c166e141ddd00
Bartek <bartlomiejtolak7@gmail.com>
2023-04-22 14:02:13 +0200

    [change]some cosmetics changes

---
+++ src/main/java/pl/sda/OrangeJavaPL2/configuration/WelcomeMessage.java
@@ -0,0 +1,14 @@
+package pl.sda.OrangeJavaPL2.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties("message")
+public class WelcomeMessage {
+
+    private String value;
+
+}
--- src/main/java/pl/sda/OrangeJavaPL2/restapi/CustomRestController.java
+++ src/main/java/pl/sda/OrangeJavaPL2/restapi/CustomRestController.java
@@ -1,14 +1,18 @@
 package pl.sda.OrangeJavaPL2.restapi;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.OrangeJavaPL2.configuration.WelcomeMessage;
 
 @RestController
+@RequiredArgsConstructor
 public class CustomRestController {
+    private final WelcomeMessage welcomeMessage;
     @GetMapping("/hello")
-    public String helloDefualt(){return "John Rambo default";}
+    public String helloDefualt(){return "John Rambo default "+welcomeMessage.getValue();}
 //    @RequestMapping(method = RequestMethod.GET,path = "/hellodef")
 //    public String helloDev1(){
 //        return "Hello default";
