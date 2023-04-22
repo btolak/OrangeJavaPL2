b948b0d66de5fa2c74eb240d921b4511a5ad0735
Bartek <bartlomiejtolak7@gmail.com>
2023-04-22 09:11:42 +0200

    [ADD] add two restcontroller class and resources for it

--- src/main/java/pl/sda/OrangeJavaPL2/restapi/CustomRestController.java
+++ src/main/java/pl/sda/OrangeJavaPL2/restapi/CustomRestController.java
@@ -3,9 +3,23 @@ package pl.sda.OrangeJavaPL2.restapi;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Profile("prd")
+
 @RestController
-public class MyFirstRestController {
+public class CustomRestController {
+
+    @GetMapping("/hello")
+    public String HelloDefault(){
+        return "John Rambo";
+    }
+
+
+    @Profile("dev")
+    @GetMapping("/hello1")
+    public String HelloDev(){
+        return "John Rambo from dev";
+    }
+
+
     @Profile("prd")
     @GetMapping("/hello2")
     public String HelloProduction(){
