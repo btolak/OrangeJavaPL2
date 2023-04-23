f5c2578615220bbace75a647104f66e919aa9b9e
Bartek <bartlomiejtolak7@gmail.com>
2023-04-23 13:30:26 +0200

    [CHANGE]change path in PostMapping

--- src/main/java/pl/sda/OrangeJavaPL2/exercise2/restApi/ArtRestController.java
+++ src/main/java/pl/sda/OrangeJavaPL2/exercise2/restApi/ArtRestController.java
@@ -1,20 +1,24 @@
 package pl.sda.OrangeJavaPL2.exercise2.restApi;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.OrangeJavaPL2.exercise2.entity.Art;
 import pl.sda.OrangeJavaPL2.exercise2.repository.ArtRepository;
+import pl.sda.OrangeJavaPL2.exercise2.service.ArtService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/arts")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class ArtRestController {
-private final ArtRepository artRepository;
-    @GetMapping
-    public List<Art> getAllArts(){return artRepository.getAllArts();}
+private final ArtService artservice;
+    @GetMapping("/arts")
+    public List<Art> getAllArts(){return artservice.getAllArts();}
+    @PostMapping(path ="/addArts")
+    public void addArt(@RequestBody Art art) {
+       artservice.addArt(art);
+    }
 
 }
