7547724d6edeb418945d4b214c022812e2fc49f3
Bartek <bartlomiejtolak7@gmail.com>
2023-05-07 13:25:05 +0200

    [add] add BakerySecurityConfig . class with spring security (authorizated)

---
+++ src/main/java/pl/sda/OrangeJavaPL2/security/BakerySecurityConfig.java
@@ -0,0 +1,20 @@
+package pl.sda.OrangeJavaPL2.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+public class BakerySecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().disable()
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
